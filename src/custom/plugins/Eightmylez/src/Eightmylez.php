ba7901f603103456f3b2c1dcc215a0916276a580
Liizzeey <lizzey@outlook.de>
2023-08-06 21:25:10 +0200

    first commit

---
+++ src/custom/plugins/Eightmylez/src/Eightmylez.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Eightmylez;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class Eightmylez extends Plugin implements ThemeInterface
+{
+}
